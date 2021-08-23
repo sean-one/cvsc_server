343419bdf1f40c7340fabfd87aaf3c2ecad345c0
sean-one <basement365@gmail.com>
2021-08-22 19:21:03 -0700

    added some error handling to the brand form

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -21,7 +21,6 @@ const validateToken = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(decoded)
         req.decodedToken = decoded;
         next()
     } catch (error) {
