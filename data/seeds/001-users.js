59da2fbbdba3ecb608f54e119dfb3f337f021018
sean-one <basement365@gmail.com>
2021-10-04 14:29:20 -0700

    update event and user seeds

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { username: 'seanone', email: 'basement365@gmail.com', avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I6XMQvy_ktdaZZ7d4pNZNwbhrCoA1SAvvaB8Mi1QA=s32-c-mo', password: '$2a$14$tswDPo02q2gg/DcwH11e3.C2sziAWdSjnBeL35bQ9L36qdWeadM9e', isAdmin: true, accounttype: 'editor' },
+        { username: 'seanone', email: 'basement365@gmail.com', avatar: 'https://coachellavalleysmokers-images.s3.amazonaws.com/23988.jpeg', password: '$2a$14$tswDPo02q2gg/DcwH11e3.C2sziAWdSjnBeL35bQ9L36qdWeadM9e', isAdmin: true, accounttype: 'editor' },
         { username: 'janet', email: 'jlopez760@aol.com', avatar: 'https://picsum.photos/100/100', password: '$2a$14$3HABR4cxzl.uP/.QosYes.Kkjnj72g9eR1Z7wkQ2413rQ8btNaZjC', isAdmin: true, accounttype: 'editor' }
       ]);
     });
