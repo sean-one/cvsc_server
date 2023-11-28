decd07ae36c20a5316515e13c96180d9ce598204
sean-one <basement365@gmail.com>
2023-11-27 17:48:34 -0800

    adjusted token errors

--- error_messages/tokenErrors.js
+++ error_messages/tokenErrors.js
@@ -1,18 +1,20 @@
 
 // used in jwt_helper -> validToken
 const tokenErrors = {
-    invalid_token: {
-        status: 401,
-        message: 'invalid token, please log in',
-        type: 'token'
-    },
+    // missing token return 401
     no_token: {
         status: 401,
         message: 'missing token, please log in',
         type: 'token'
     },
+    // invalid or expired return 403
+    invalid_token: {
+        status: 403,
+        message: 'invalid token, please log in',
+        type: 'token'
+    },
     TokenExpiredError: {
-        status: 401,
+        status: 403,
         message: 'token has expired, please log in',
         type: 'token'
     },
