a043ad9415ab0f2db0dcb54d8aeccf4769feb960
sean-one <basement365@gmail.com>
2024-03-17 20:17:16 -0700

    working on event returns

--- data/models/event.js
+++ data/models/event.js
@@ -18,35 +18,53 @@ module.exports = {
 // .get('EVENTS/business/:business_id') - returns array of ACTIVE events for specific business id
 async function getBusinessEvents(business_id) {
     try {
-        return await db('events')
-            // Ensure eventdate and eventstart are in the future
-            .whereRaw(`(events.eventdate || ' ' || LPAD(events.eventstart::text, 4, '0')::time)::timestamp >= CURRENT_TIMESTAMP`)
-            // Match either brand_id or venue_id with the provided business_id
-            .andWhere('events.host_business', '=', business_id)
-            // Remove inactive events from event list return
-            .andWhere({ active_event: true })
-            .join('businesses', 'events.host_business', '=', 'businesses.id')
-            .join('users', 'events.created_by', '=', 'users.id')
+        // Query for direct events
+        const directEventsQuery = db('events as e')
+            .select('e.id')
+            .where('e.host_business', '=', business_id);
+
+        // Query for events tagged with the business_id
+        const taggedEventsQuery = db('business_tags as bt')
+            .select('bt.event_id as id')
+            .where('bt.business_id', '=', business_id)
+            .andWhereNotNull('bt.approved_by');
+
+        // Combine the queries to get a unified list of event IDs
+        const combinedEventIDsQuery = directEventsQuery.union([taggedEventsQuery]);
+
+        console.log(combinedEventIDsQuery)
+        // Fetch the complete event records matching the combined list of event IDs
+        return await db('events as e')
+            .joinRaw(`JOIN (${combinedEventIDsQuery.toQuery()}) AS combined ON e.id = combined.id`)
+            .leftJoin('users as u', 'e.created_by', 'u.id')
+            .leftJoin('businesses as b', 'e.host_business', 'b.id')
+            .leftJoin('business_tags as bt', 'e.id', 'bt.event_id')
             .select([
-                'events.id as event_id',
-                'events.eventname',
-                'events.place_id',
-                'events.formatted_address',
-                'events.eventdate',
-                'events.eventstart',
-                'events.eventend',
-                'events.eventmedia',
-                'events.host_business',
-                'businesses.business_name',
-                'events.details',
-                'events.active_event',
-    
-                'events.created_by',
-                'users.username as event_creator'
+                'e.id as event_id',
+                'e.eventname',
+                'e.place_id',
+                'e.formatted_address',
+                'e.eventdate',
+                'e.eventstart',
+                'e.eventend',
+                'e.eventmedia',
+                'e.details',
+                'e.host_business',
+                'e.created_by',
+                'e.active_event',
+                'u.username as event_creator',
+                'b.business_name',
+                // Updated ARRAY_AGG to include only approved business tags
+                db.raw('ARRAY_AGG(bt.business_id) FILTER (WHERE bt.business_id IS NOT NULL AND bt.approved_by IS NOT NULL) as tags'),
             ])
+            .whereRaw(`(e.eventdate || ' ' || LPAD(e.eventstart::text, 4, '0')::time)::timestamp >= CURRENT_TIMESTAMP`)
+            .andWhere({ active_event: true })
+            .groupBy('e.id', 'u.username', 'b.business_name')
             // Order by combined timestamp of eventdate and reformatted eventstart
-            .orderByRaw(`(events.eventdate || ' ' || LPAD(events.eventstart::text, 4, '0')::time)::timestamp`);
+            .orderByRaw(`(e.eventdate || ' ' || LPAD(e.eventstart::text, 4, '0')::time)::timestamp`);
+
     } catch (error) {
+        console.log(error)
         console.error('Error fetching business events:', Object.keys(error));
         throw new Error('fetch_business_events_server_error');
     }
@@ -97,32 +115,32 @@ async function getEventRelatedEvents(event_id) {
 // .get('EVENTS/user/:user_id')
 async function getUserEvents(user_id) {
     try {
-        return await db('events')
-            .where({ created_by: user_id })
-            .andWhere('events.eventdate', '>=', new Date())
-            .join('users', 'events.created_by', '=', 'users.id')
-            .join('businesses', 'events.host_business', '=', 'businesses.id')
-            .select(
-                [
-                    'events.id as event_id',
-                    'events.eventname',
-                    'events.place_id',
-                    'events.formatted_address',
-                    'events.eventdate',
-                    'events.eventstart',
-                    'events.eventend',
-                    'events.eventmedia',
-                    'events.host_business',
-                    'businesses.business_name',
-                    'events.details',
-
-
-                    'events.active_event',
-                    'events.created_by',
-                    'users.username as event_creator'
-                ]
-            )
-            .orderByRaw(`(events.eventdate || ' ' || LPAD(events.eventstart::text, 4, '0')::time)::timestamp`)
+        return await db('events as e')
+            .leftJoin('users as u', 'e.created_by', 'u.id')
+            .leftJoin('businesses as b', 'e.host_business', 'b.id')
+            .leftJoin('business_tags as bt', 'e.id', 'bt.event_id')
+            .select([
+                'e.id as event_id',
+                'e.eventname',
+                'e.place_id',
+                'e.formatted_address',
+                'e.eventdate',
+                'e.eventstart',
+                'e.eventend',
+                'e.eventmedia',
+                'e.details',
+                'e.host_business',
+                'e.created_by',
+                'e.active_event',
+                'u.username as event_creator',
+                'b.business_name',
+                // Updated ARRAY_AGG to include only approved business tags
+                db.raw('ARRAY_AGG(bt.business_id) FILTER (WHERE bt.business_id IS NOT NULL AND bt.approved_by IS NOT NULL) as tags'),
+            ])
+            .whereRaw(`(e.eventdate || ' ' || LPAD(e.eventstart::text, 4, '0')::time)::timestamp >= CURRENT_TIMESTAMP`)
+            .andWhere({ 'e.created_by': user_id })
+            .groupBy('e.id', 'u.username', 'b.business_name')
+            .orderByRaw(`(e.eventdate || ' ' || LPAD(e.eventstart::text, 4, '0')::time)::timestamp`)
     } catch (error) {
         console.error('Error fetching related user events:', error);
         throw new Error('fetch_user_events_server_error');
