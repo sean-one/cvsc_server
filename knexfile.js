8d27950efea180f6f71ba1629ddebc6041a869e6
sean-one <basement365@gmail.com>
2024-05-17 03:51:52 -0700

    quick change

--- knexfile.js
+++ knexfile.js
@@ -47,7 +47,6 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      ssl: { rejectUnauthorized: false },
     },
     useNullAsDefault: true,
     pool: {
