d721a548b94cbeba8e9903b77136a8f67527a9e8
sean-one <basement365@gmail.com>
2024-05-17 03:30:07 -0700

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -37,6 +37,25 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: { rejectUnauthorized: false },
+    },
+    migration: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
+
 
 };
