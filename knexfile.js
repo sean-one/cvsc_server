4f941c043e47b711db1e1e9c5eae7a0dab7dcd96
sean-one <basement365@gmail.com>
2024-05-17 04:06:27 -0700

    copied over

--- knexfile.js
+++ knexfile.js
@@ -45,19 +45,20 @@ module.exports = {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      ssl: { rejectUnauthorized: false },
     },
     useNullAsDefault: true,
     pool: {
       min: 2,
       max: 10,
     },
-    migration: {
-      directory: './data/migrations/',
+    migrations: {
+      directory: './data/migrations',
     },
     seeds: {
-      directory: './data/migrations/',
+      directory: './data/seeds',
     },
   },
 
