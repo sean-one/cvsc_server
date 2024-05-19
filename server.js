5f011f8062fb48c33bcc50ab5e240c209dafb799
sean-one <basement365@gmail.com>
2024-05-18 23:14:20 -0700

    updated server file

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.use(cookieParser())
 const dynamoDBClient = new DynamoDBClient({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESSKEYID,
+        secretAccessKey: process.env.AWS_SECRETACCESSKEY,
     }
 
 })
